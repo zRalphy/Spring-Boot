dc7478f14f179b9d6605d065837bac080672a365
zRalphy <rafalmec@gmail.com>
2021-08-31 21:38:19 +0200

    Base class of application

--- src/main/java/pl/rafalmecwaldowski/MyAudi/MyAudiApplication.java
+++ src/main/java/pl/rafalmecwaldowski/MyAudi/MyAudiApplication.java
@@ -2,12 +2,33 @@ package pl.rafalmecwaldowski.MyAudi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.rafalmecwaldowski.MyAudi.car.Car;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class MyAudiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAudiApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Car> addCar(){
+		return List.of(
+				new Car(
+						1L,
+						"Audi",
+						"A5",
+						"WVWZZZ1KZ7W518416",
+						LocalDate.of(2010, Month.FEBRUARY, 12),
+						185000
+				)
+		);
+	}
 }
---
+++ src/main/java/pl/rafalmecwaldowski/MyAudi/car/Car.java
@@ -0,0 +1,89 @@
+package pl.rafalmecwaldowski.MyAudi.car;
+
+import java.time.LocalDate;
+
+public class Car {
+    private Long id;
+    private String brand;
+    private String model;
+    private String vinNumber;
+    private LocalDate dof;
+    private Integer mileage;
+
+    public Car(Long id, String brand, String model, String vinNumber, LocalDate dof, Integer mileage) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.vinNumber = vinNumber;
+        this.dof = dof;
+        this.mileage = mileage;
+    }
+
+    public Car(String brand, String model, String vinNumber, LocalDate dof, Integer mileage) {
+        this.brand = brand;
+        this.model = model;
+        this.vinNumber = vinNumber;
+        this.dof = dof;
+        this.mileage = mileage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVinNumber() {
+        return vinNumber;
+    }
+
+    public void setVinNumber(String vinNumber) {
+        this.vinNumber = vinNumber;
+    }
+
+    public LocalDate getDof() {
+        return dof;
+    }
+
+    public void setDof(LocalDate dof) {
+        this.dof = dof;
+    }
+
+    public Integer getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Integer mileage) {
+        this.mileage = mileage;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", vinNumber='" + vinNumber + '\'' +
+                ", dof=" + dof +
+                ", mileage=" + mileage +
+                '}';
+    }
+}
