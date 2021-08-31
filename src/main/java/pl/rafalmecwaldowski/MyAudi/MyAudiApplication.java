3ea4cb7a81c7573c864ffdf630fbf835004ea210
zRalphy <rafalmec@gmail.com>
2021-08-31 21:00:03 +0200

    Empty Spring Boot project

---
+++ src/main/java/pl/rafalmecwaldowski/MyAudi/MyAudiApplication.java
@@ -0,0 +1,13 @@
+package pl.rafalmecwaldowski.MyAudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAudiApplication.class, args);
+	}
+
+}
